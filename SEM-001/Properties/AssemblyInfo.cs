5cab37db92d9fe8f7580c0b1088ec18a13cdddc5
Roman Danilov <rdanilov75@gmail.com>
2019-04-25 18:33:37 +0300

    Добавлен копирайт LIBFL

--- SEM-001/Properties/AssemblyInfo.cs
+++ SEM-001/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SEM_001")]
-[assembly: AssemblyCopyright("© , 2019")]
+[assembly: AssemblyCopyright("© LIBFL, 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
